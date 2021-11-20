fe2370d2c6ab7a4a4c433938de1b0c4b95ffc0c7
NonSpeakPeach <245149633@qq.com>
2021-11-20 15:54:25 +0800

    家里的测试

--- src/main/java/bbs/ApplicationStart.java
+++ src/main/java/bbs/ApplicationStart.java
@@ -8,5 +8,4 @@ public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
     }
-
 }
--- src/main/java/bbs/controller/HomeController.java
+++ src/main/java/bbs/controller/HomeController.java
@@ -3,10 +3,28 @@ package bbs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 主要控制未登录情况下的数据与页面
+ */
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping({"/","index"})
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @RequestMapping("/resource")
+    public String resource(){
+        return "resource";
     }
 }
